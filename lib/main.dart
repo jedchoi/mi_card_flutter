909802de3b81ea61862bfc39930012fabebe0ad4
JaeHyuk Choi <jaehyuk1220@gmail.com>
2022-08-29 16:57:06 +0900

    Card를 사용하여 전화번호, 이메일 추가

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,11 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             CircleAvatar(
               radius: 50.0,
+              backgroundColor: Colors.red,
               backgroundImage: AssetImage('images/zed.jpeg'),
             ),
             Text(
@@ -36,6 +38,51 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   letterSpacing: 2.5),
             ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+82-10-7777-7777',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900),
+                  ),
+                )),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  title: Text(
+                    'jedchoi@email.com',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900),
+                  ),
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                )),
           ],
         )),
       ),
